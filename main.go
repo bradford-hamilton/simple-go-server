493e95f2d91ea231d27724af7a7fdad980cd9d1b
Bradford Lamson-Scribner <brad.lamson@gmail.com>
2017-09-18 11:54:11 -0600

    [refactor] pull out the todo struct into its own file

--- main.go
+++ main.go
@@ -3,18 +3,9 @@ package main
 import (
 	"log"
 	"net/http"
-	"time"
 )
 
 func main() {
 
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
-
-type Todo struct {
-	Name      string    `json:"name"`
-	Completed bool      `json:"completed"`
-	Due       time.Time `json:"due"`
-}
-
-type Todos []Todo
